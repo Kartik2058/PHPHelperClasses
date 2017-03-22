6f3750d3be0682795aa034b69a102e82f2a5f98d
Kartik2058 <kartik2058@gmail.com>
2017-03-22 14:17:09 +0530

    Created connection to the database usnig PDO

--- Database.php
+++ Database.php
@@ -1,5 +1,14 @@
 <?php
 
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "test");
+
 class Database {
+    
+    public function __construct() {
+        $this->connection = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME,DB_USER,DB_PASS);
+    }
     
 }
